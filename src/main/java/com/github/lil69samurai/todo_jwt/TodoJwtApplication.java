226490c9788a9695d5c7e209218e6160c0fcbfe8
lil69samurai <mmcgrady1tw@gmail.com>
2026-04-12 23:16:52 +0900

    Initial commit: 專案初始化與 MySQL 連線、實體類別建立

---
+++ src/main/java/com/github/lil69samurai/todo_jwt/TodoJwtApplication.java
@@ -0,0 +1,14 @@
+package com.github.lil69samurai.todo_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJwtApplication.class, args);
+	}
+
+}
+https://github.com/lil69samurai/todo-jwt.git
---
+++ src/test/java/com/github/lil69samurai/todo_jwt/TodoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lil69samurai.todo_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
